992465a39d7e8d0aec4bd07593a9343b295050c8
Madawa Soysa <madawa.rc@gmail.com>
2015-02-19 10:29:07 +0530

    add org.wso2.carbon.registry.extensions jar dependency

--- modules/samples/handler/src/src/main/java/org/wso2/carbon/registry/samples/handler/util/CommonConstants.java
+++
@@ -1,8 +0,0 @@
-package org.wso2.carbon.registry.samples.handler.util;
-
-public class CommonConstants {
-
-    public static final String DEFAULT_SWAGGER_VERSION = "2.0";
-
-	public static final String METADATA_NAMESPACE_URL = "http://www.wso2.org/governance/metadata";
-}
